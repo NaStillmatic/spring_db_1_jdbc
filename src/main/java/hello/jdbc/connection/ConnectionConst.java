1ba98f248cf4454809e7443f47334be556790f74
Joe <nastillmatic@gmail.com>
2024-03-13 17:53:31 +0900

    Section 1- 데이터베이스 연결

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static String USERNAME = "sa";
+    public static String PASSWORD = "";
+}
