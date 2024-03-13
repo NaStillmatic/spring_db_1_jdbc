6b338d7cb2ccef8a010ce33da9657d24dc5ef79d
Joe <nastillmatic@gmail.com>
2024-03-13 18:55:04 +0900

    Section 1 - JDBC 개발 - 조회

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,29 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
-    void save() throws SQLException {
+    void crud() throws SQLException {
 
-        Member member = new Member("memberV0", 10000);
+        // save
+        Member member = new Member("memberV4", 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember);
+        log.info("member.equals findMember {}", member.equals(findMember)); // lombok을 써서 가능하다.
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
